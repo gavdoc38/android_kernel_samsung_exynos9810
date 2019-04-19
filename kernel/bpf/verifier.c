efb6f90789927d39d0e0b30ff84598424f597314
Alexei Starovoitov <ast@kernel.org>
2019-04-19 07:44:55 -0700

    BACKPORT: bpf: drop bpf_verifier_lock
    Drop bpf_verifier_lock for root to avoid being DoS-ed by unprivileged.
    The BPF verifier is now fully parallel.
    All unpriv users are still serialized by bpf_verifier_lock to avoid
    exhausting kernel memory by running N parallel verifications.
    
    Change-Id: I3debaa6e0d9dd338cdd1ce17f28cf30e0d4fb0c1
    Signed-off-by: Alexei Starovoitov <ast@kernel.org>
    Signed-off-by: Daniel Borkmann <daniel@iogearbox.net>
    
    (cherry picked from commit 524e03dc3e93fcfd5028bdd720e188764240fbaa)
    [Mathias Gluszczynski: Removed the global verifier lock after moving
    mutable verifier state into the environment.]
    Signed-off-by: Mathias Gluszczynski <admin@krazey.de>

--- kernel/bpf/verifier.c
+++ kernel/bpf/verifier.c
@@ -7401,6 +7401,7 @@ int bpf_check(struct bpf_prog **prog, union bpf_attr *attr,
 	struct bpf_verifier_env *env;
 	struct bpf_verifier_log *log;
 	int ret = -EINVAL;
+	bool is_priv;
 
 	/* 'struct bpf_verifier_env' can be global, but since it's not small,
 	 * allocate/free it every time bpf_check() is called
@@ -7417,9 +7418,11 @@ int bpf_check(struct bpf_prog **prog, union bpf_attr *attr,
 		goto err_free_env;
 	env->prog = *prog;
 	env->ops = bpf_verifier_ops[env->prog->type];
+	is_priv = capable(CAP_SYS_ADMIN);
 
 	/* grab the mutex to protect few globals used by verifier */
-	mutex_lock(&bpf_verifier_lock);
+	if (!is_priv)
+		mutex_lock(&bpf_verifier_lock);
 
 	if (attr->log_level || attr->log_buf || attr->log_size) {
 		/* user requested verbose verifier output
@@ -7446,6 +7449,8 @@ int bpf_check(struct bpf_prog **prog, union bpf_attr *attr,
 			goto err_unlock;
 	}
 
+	env->allow_ptr_leaks = is_priv;
+
 	ret = replace_map_fd_with_map_ptr(env);
 	if (ret < 0)
 		goto skip_full_check;
@@ -7456,8 +7461,6 @@ int bpf_check(struct bpf_prog **prog, union bpf_attr *attr,
 	ret = -ENOMEM;
 	if (!env->explored_states)
 		goto skip_full_check;
-
-	env->allow_ptr_leaks = capable(CAP_SYS_ADMIN);
 
 	ret = check_cfg(env);
 	if (ret < 0)
@@ -7536,7 +7539,8 @@ err_release_maps:
 		release_maps(env);
 	*prog = env->prog;
 err_unlock:
-	mutex_unlock(&bpf_verifier_lock);
+	if (!is_priv)
+		mutex_unlock(&bpf_verifier_lock);
 	vfree(env->insn_aux_data);
 err_free_env:
 	kfree(env);
