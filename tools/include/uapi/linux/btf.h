18a1bf52321278ab8c61c5f9c4d6c7259c453eb0
Gary Lin <glin@suse.com>
2019-05-13 17:45:48 +0800

    BACKPORT: bpf: Finish BTF UAPI header synchronization
    [ Upstream commits
    948dc8c99a22d6bdcb34c194cde392e1a125928a
    2d3eb67f64ec317bbfe340cfcc8325d40a6ff317 ]
    
    Protect the BTF_INT_OFFSET argument correctly and copy the function
    linkage enum into the tools UAPI header. The kernel and tools copies now
    match each other and Linux 5.15 byte for byte.
    
    [Mathias Gluszczynski: apply the missing header-only changes. The
    related global-function verifier support is already present.]
    
    Signed-off-by: Gary Lin <glin@suse.com>
    Signed-off-by: Alexei Starovoitov <ast@kernel.org>
    Acked-by: Song Liu <songliubraving@fb.com>
    Signed-off-by: Daniel Borkmann <daniel@iogearbox.net>
    Signed-off-by: Mathias Gluszczynski <admin@krazey.de>

--- tools/include/uapi/linux/btf.h
+++ tools/include/uapi/linux/btf.h
@@ -84,7 +84,7 @@ struct btf_type {
  * is the 32 bits arrangement:
  */
 #define BTF_INT_ENCODING(VAL)	(((VAL) & 0x0f000000) >> 24)
-#define BTF_INT_OFFSET(VAL)	(((VAL  & 0x00ff0000)) >> 16)
+#define BTF_INT_OFFSET(VAL)	(((VAL) & 0x00ff0000) >> 16)
 #define BTF_INT_BITS(VAL)	((VAL)  & 0x000000ff)
 
 /* Attributes stored in the BTF_INT_ENCODING */
@@ -145,6 +145,12 @@ enum {
 	BTF_VAR_STATIC = 0,
 	BTF_VAR_GLOBAL_ALLOCATED = 1,
 	BTF_VAR_GLOBAL_EXTERN = 2,
+};
+
+enum btf_func_linkage {
+	BTF_FUNC_STATIC = 0,
+	BTF_FUNC_GLOBAL = 1,
+	BTF_FUNC_EXTERN = 2,
 };
 
 /* BTF_KIND_VAR is followed by a single "struct btf_var" to describe
