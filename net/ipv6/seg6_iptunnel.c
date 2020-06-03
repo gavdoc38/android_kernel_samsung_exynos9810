bf2e46f8c24adc2bcdb1b17dd4f38d261d1bc9a4
Ahmed Abdelsalam <ahabdels@gmail.com>
2020-06-03 06:54:42 +0000

    BACKPORT: seg6: Harden SRH validation for BPF
    [ Upstream commit bb986a50421a11bf31a81afb15b9b8f45a4a3a11 ]
    
    Distinguish reduced data-plane segment routing headers from headers
    injected by BPF or netlink. Validate first_segment against the actual
    header capacity so malformed inputs cannot walk beyond the allocation.
    
    [Mathias Gluszczynski: the legacy IPv6 socket-option path does not
    accept SRH type 4, so update only the BPF and LWT consumers present in
    this tree. Exclude userspace selftests.]
    
    Reported-by: syzbot+e8c028b62439eac42073@syzkaller.appspotmail.com
    Reported-by: YueHaibing <yuehaibing@huawei.com>
    Signed-off-by: Ahmed Abdelsalam <ahabdels@gmail.com>
    Signed-off-by: David S. Miller <davem@davemloft.net>
    Signed-off-by: Mathias Gluszczynski <admin@krazey.de>

--- net/ipv6/seg6_iptunnel.c
+++ net/ipv6/seg6_iptunnel.c
@@ -374,7 +374,8 @@ static int seg6_build_state(struct net_device *dev, struct nlattr *nla,
 	}
 
 	/* verify that SRH is consistent */
-	if (!seg6_validate_srh(tuninfo->srh, tuninfo_len - sizeof(*tuninfo)))
+	if (!seg6_validate_srh(tuninfo->srh,
+			       tuninfo_len - sizeof(*tuninfo), false))
 		return -EINVAL;
 
 	newts = lwtunnel_state_alloc(tuninfo_len + sizeof(*slwt));
