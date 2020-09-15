9831c0b1853b56ce1c03f69ae0d5c6cf65ff2e30
Martin KaFai Lau <kafai@fb.com>
2020-09-15 11:29:59 -0700

    BACKPORT: bpf: Bpf_skc_to_* helpers require a NULL check on sk
    [ Upstream commit 8c33dadc3e0eef1599811a55d748a0b95da0317d ]
    
    The bpf_skc_to_* type casting helpers are available to
    BPF_PROG_TYPE_TRACING.  The traced PTR_TO_BTF_ID may be NULL.
    For example, the skb->sk may be NULL.  Thus, these casting helpers
    need to check "!sk" also and this patch fixes them.
    
    [Mathias Gluszczynski: Keep the configuration guards used by the
    preceding Exynos9810 backport while adding the NULL checks.]
    
    Fixes: 0d4fad3e57df ("bpf: Add bpf_skc_to_udp6_sock() helper")
    Fixes: 478cfbdf5f13 ("bpf: Add bpf_skc_to_{tcp, tcp_timewait,
            tcp_request}_sock() helpers")
    Fixes: af7ec1383361 ("bpf: Add bpf_skc_to_tcp6_sock() helper")
    Signed-off-by: Martin KaFai Lau <kafai@fb.com>
    Signed-off-by: Alexei Starovoitov <ast@kernel.org>
    Acked-by: Yonghong Song <yhs@fb.com>
    Acked-by: Song Liu <songliubraving@fb.com>
    Link: https://lore.kernel.org/bpf/20200915182959.241101-1-kafai@fb.com
    Signed-off-by: Mathias Gluszczynski <admin@krazey.de>

--- net/core/filter.c
+++ net/core/filter.c
@@ -6190,7 +6190,7 @@ BPF_CALL_1(bpf_skc_to_tcp6_sock, struct sock *, sk)
 {
 	/* tcp6_sock type is not generated in dwarf and hence BTF. */
 	BTF_TYPE_EMIT(struct tcp6_sock);
-	if (sk_fullsock(sk) && sk->sk_protocol == IPPROTO_TCP &&
+	if (sk && sk_fullsock(sk) && sk->sk_protocol == IPPROTO_TCP &&
 	    sk->sk_family == AF_INET6)
 		return (unsigned long)sk;
 
@@ -6208,7 +6208,7 @@ const struct bpf_func_proto bpf_skc_to_tcp6_sock_proto = {
 
 BPF_CALL_1(bpf_skc_to_tcp_sock, struct sock *, sk)
 {
-	if (sk_fullsock(sk) && sk->sk_protocol == IPPROTO_TCP)
+	if (sk && sk_fullsock(sk) && sk->sk_protocol == IPPROTO_TCP)
 		return (unsigned long)sk;
 
 	return (unsigned long)NULL;
@@ -6226,12 +6226,14 @@ const struct bpf_func_proto bpf_skc_to_tcp_sock_proto = {
 BPF_CALL_1(bpf_skc_to_tcp_timewait_sock, struct sock *, sk)
 {
 #ifdef CONFIG_INET
-	if (sk->sk_prot == &tcp_prot && sk->sk_state == TCP_TIME_WAIT)
+	if (sk && sk->sk_prot == &tcp_prot &&
+	    sk->sk_state == TCP_TIME_WAIT)
 		return (unsigned long)sk;
 #endif
 
 #if IS_BUILTIN(CONFIG_IPV6)
-	if (sk->sk_prot == &tcpv6_prot && sk->sk_state == TCP_TIME_WAIT)
+	if (sk && sk->sk_prot == &tcpv6_prot &&
+	    sk->sk_state == TCP_TIME_WAIT)
 		return (unsigned long)sk;
 #endif
 
@@ -6250,12 +6252,13 @@ const struct bpf_func_proto bpf_skc_to_tcp_timewait_sock_proto = {
 BPF_CALL_1(bpf_skc_to_tcp_request_sock, struct sock *, sk)
 {
 #ifdef CONFIG_INET
-	if (sk->sk_prot == &tcp_prot && sk->sk_state == TCP_NEW_SYN_RECV)
+	if (sk && sk->sk_prot == &tcp_prot &&
+	    sk->sk_state == TCP_NEW_SYN_RECV)
 		return (unsigned long)sk;
 #endif
 
 #if IS_BUILTIN(CONFIG_IPV6)
-	if (sk->sk_prot == &tcpv6_prot &&
+	if (sk && sk->sk_prot == &tcpv6_prot &&
 	    sk->sk_state == TCP_NEW_SYN_RECV)
 		return (unsigned long)sk;
 #endif
@@ -6276,7 +6279,7 @@ BPF_CALL_1(bpf_skc_to_udp6_sock, struct sock *, sk)
 {
 	/* udp6_sock is not otherwise guaranteed to be emitted into BTF. */
 	BTF_TYPE_EMIT(struct udp6_sock);
-	if (sk_fullsock(sk) && sk->sk_protocol == IPPROTO_UDP &&
+	if (sk && sk_fullsock(sk) && sk->sk_protocol == IPPROTO_UDP &&
 	    sk->sk_type == SOCK_DGRAM && sk->sk_family == AF_INET6)
 		return (unsigned long)sk;
 
