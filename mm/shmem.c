19610d764e41da432aa10da8af3a8c5778106804
gavdov38 <gavin.a.docherty@gmail.com>
2026-07-03 23:56:47 +0100

    mm: susfs: implement sus_memfd sidex15: "This feature is based on the susfs v1.3.8 and ported to the recent susfs v2.2.0 codebase.
    This is highly experimental so put this feature with so much grain of salt...
    
    I haven't test it yet as I don't have memfd: mappings in /proc/maps but the execution via userspace binary works."

--- mm/shmem.c
+++ mm/shmem.c
@@ -78,6 +78,10 @@ static struct vfsmount *shm_mnt;
 #include <asm/uaccess.h>
 #include <asm/pgtable.h>
 
+#ifdef CONFIG_KSU_SUSFS
+#include <linux/susfs.h>
+#endif
+
 #include "internal.h"
 
 #define BLOCKS_PER_PAGE  (PAGE_SIZE/512)
@@ -3731,6 +3735,13 @@ SYSCALL_DEFINE2(memfd_create,
 		error = -EFAULT;
 		goto err_name;
 	}
+
+#ifdef CONFIG_KSU_SUSFS_SUS_MEMFD
+	if (susfs_sus_memfd(name)) {
+		error = -EFAULT;
+		goto err_name;
+	}
+#endif
 
 	fd = get_unused_fd_flags((flags & MFD_CLOEXEC) ? O_CLOEXEC : 0);
 	if (fd < 0) {
