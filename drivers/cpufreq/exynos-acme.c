36edb090c71265b31e5c7c603a73ef60447b2d23
Mathias Gluszczynski <admin@krazey.de>
2026-07-15 03:15:45 -0500

    [exynos9810] cpufreq: honor DTS limits with hotplug
    The Exynos hotplug governor does not replace cpufreq domain limits.
    Keep applying max-freq from the device tree when it is enabled.
    
    This restores the 1.95 GHz LITTLE OPP and prevents the unused
    2.70 GHz BIG OPPs from being exposed to cpufreq and userspace.
    
    Signed-off-by: Mathias Gluszczynski <admin@krazey.de>

--- drivers/cpufreq/exynos-acme.c
+++ drivers/cpufreq/exynos-acme.c
@@ -1120,10 +1120,8 @@ static __init int init_domain(struct exynos_cpufreq_domain *domain,
 	 * tree and CAL. In case of min-freq, min frequency is selected
 	 * to bigger one.
 	 */
-#ifndef CONFIG_EXYNOS_HOTPLUG_GOVERNOR
 	if (!of_property_read_u32(dn, "max-freq", &val))
 		domain->max_freq = val;
-#endif
 	if (!of_property_read_u32(dn, "min-freq", &val))
 		domain->min_freq = max(domain->min_freq, val);
 #ifdef CONFIG_SEC_PM
